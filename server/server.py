ed9c0b5cddf9c9d38f99b60d0ac20f10f09900a4
Ubuntu <ubuntu@ip-172-31-9-128.ec2.internal>
2022-07-16 13:21:03 +0000

    modify

--- server/server.py
+++ server/server.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
-import util
+import func
 
 app = Flask(__name__)
+
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'locations': util.get_location_names()
+        'locations': func.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
+
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
@@ -20,13 +22,14 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': func.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
+
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
+    print("Starting Python Flask Server For Bengluru Home Price Prediction...")
+    func.load_saved_artifacts()
     app.run()
